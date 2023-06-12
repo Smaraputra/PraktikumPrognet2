b9d12678b863375f06d9961f93b9da5d9e43d31a
Smaraputra <smara.putra2001@gmail.com>
2023-06-12 23:36:27 +0800

    Feat: bug fix register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,7 +2,8 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-6">
+        <div class="col-md-2"></div>
+        <div class="col-md-8">
             <div class="card">
                 <div class="card-header text-center">
                     <h2>{{ __('Register') }}</h2>
@@ -84,6 +85,7 @@
                 </div>
             </div>
         </div>
+        <div class="col-md-2"></div>
     </div>
 </div>
 @endsection
